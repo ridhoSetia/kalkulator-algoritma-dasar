092130b23baff6a60870185b5310048c87675941
ridhoSetia <ridho.setiawan24406@gmail.com>
2025-10-24 07:02:43 +0800

    tampilkan hasil fungsi

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def hitung_luas_persegi(sisi):
 
 def main():
     print("Selamat datang di Kalkulator Bangun Datar!")
+    hitung_persegi = hitung_luas_persegi(4)
+    print(hitung_persegi)
 
 if __name__ == "__main__":
     main()
