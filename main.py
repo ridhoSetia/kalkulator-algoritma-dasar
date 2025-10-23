4805da8b351077b6a5d707440fb13c35acc97910
DwikiAprilianAryanda <181479443+DwikiAprilianAryanda@users.noreply.github.com>
2025-10-24 06:30:02 +0700

    feat: nambah luas segitiga

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ def hitung_volume_persegi(sisi):
     print(f"Menghitung volume persegi dengan sisi {sisi}...")
     return sisi**3
 
+def hitung_luas_segitiga(alas, tinggi):
+    print(f"Menghitung volume persegi dengan alas: {alas} dan tinggi: {tinggi}")
+    return (0.5)*alas*tinggi
+
 def main():
     print("Selamat datang di Kalkulator Bangun Datar!")
     hitung_persegi = hitung_luas_persegi(4)
     print(hitung_persegi)
+    print(hitung_volume_persegi(4))
+    print(hitung_luas_segitiga(10, 5))
 
 if __name__ == "__main__":
     main()
