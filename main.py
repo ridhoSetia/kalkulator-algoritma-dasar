8e7a266f994466b8c0d03349da2646872167809c
ridhoSetia <ridho.setiawan24406@gmail.com>
2025-10-24 06:42:46 +0800

    add: struktur program

---
+++ main.py
@@ -0,0 +1,9 @@
+def hitung_luas_persegi(sisi):
+    # TODO: Implementasi oleh Lian
+    pass
+
+def main():
+    print("Selamat datang di Kalkulator Bangun Datar!")
+
+if __name__ == "__main__":
+    main()
