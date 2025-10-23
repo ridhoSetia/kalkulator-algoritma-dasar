76ee1a836d80426ed3b9d384cc6dd3ccbcd603d4
ridhoSetia <ridho.setiawan24406@gmail.com>
2025-10-24 07:06:12 +0800

    feat: hitung volume persegi

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def hitung_luas_persegi(sisi):
     print(f"Menghitung luas persegi dengan sisi {sisi}...")
     return sisi * sisi
 
+def hitung_volume_persegi(sisi):
+    print(f"Menghitung volume persegi dengan sisi {sisi}...")
+    return sisi**3
+
 def main():
     print("Selamat datang di Kalkulator Bangun Datar!")
     hitung_persegi = hitung_luas_persegi(4)
